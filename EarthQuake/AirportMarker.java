31886fc86376f0782eb9930a169eb77fa0c8e5cc
Shivam Verma <shivamv0897@gmail.com>
2017-01-24 01:52:06 +0530

    Add files via upload

---
+++ EarthQuake/AirportMarker.java
@@ -0,0 +1,60 @@
+package EarthQuake;
+
+import java.util.List;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+public class AirportMarker extends CommonMarker {
+	public static List<SimpleLinesMarker> routes;
+	public static int TRI_SIZE = 5;
+	public AirportMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+	
+	}
+	
+	@Override
+	public void drawMarker(PGraphics pg, float x, float y) {
+		pg.fill(0,255,0);
+		pg.ellipse(x, y, 5, 5);
+		
+		
+	}
+
+	@Override
+	public void showTitle(PGraphics pg, float x, float y) {
+		String name = getCity() + " " + getCountry() + " ";
+		String pop = "Distance: " + getDistance() + " Million";
+		
+		pg.pushStyle();
+		
+		pg.fill(255, 255, 255);
+		pg.textSize(12);
+		pg.rectMode(PConstants.CORNER);
+		pg.rect(x, y-TRI_SIZE-39, Math.max(pg.textWidth(name), pg.textWidth(pop)) + 6, 39);
+		pg.fill(0, 0, 0);
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.text(name, x+3, y-TRI_SIZE-33);
+		pg.text(pop, x+3, y - TRI_SIZE -18);
+		
+		pg.popStyle();
+		
+	}
+	private String getCity()
+	{
+		return getStringProperty("name");
+	}
+	
+	private String getCountry()
+	{
+		return getStringProperty("country");
+	}
+	private String getDistance()
+	{
+		return getStringProperty("distance");
+	}
+	
+}
